c0f12af1bc3771573df7d1890a397f56ab6adbda
Marcio Pessoa <marcio.pessoa@sciemon.com>
2017-11-03 19:47:57 -0200

    Configuration script

---
+++ configure.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+# 
+# configure.sh
+# 
+# Description:
+#   Automatic configuration script.
+# 
+# Author: Márcio Pessoa <marcio.pessoa@sciemon.com>
+# Contributors: none
+# 
+# Change log:
+# 2017-09-12
+#         Tested on: Grbl 0.9j.
+#         Enable: COREXY.
+#         Change: HOMING_CYCLE_0 to support COREXY.
+#         Disable: HOMING_CYCLE_1 to support COREXY.
+#         Change: SPINDLE_MAX_RPM to low rotation (tool speed).
+#
+# 2017-09-11
+#         Bug fix: Minor code corrections.
+#
+# 2016-08-09
+#         Project definition.
+#
+
+# Variables
+action=$1
+readonly WORKDIR="grbl"
+# readonly WORKDIR="grbl-servo"
+readonly LIBRARY_PATH="$HOME/Development/Escriba/Mark_I/$WORKDIR"
+readonly ARDUINO_PATH="$HOME/Development/Arduino/libraries/grbl"
+
+check_return() {
+  return=$1
+  if [ "$return" -eq 0 ]; then
+    echo "Done."
+  else
+    echo "Fail."
+    exit 1
+  fi
+}
+
+get_updates() {
+  file=$1
+  echo "Getting firmware updates to $WORKDIR $(git show-branch)... \c"
+  git checkout -- "$file"  # Restore original file
+  git merge  # Get updates from Github
+}
+
+unlink "$ARDUINO_PATH" >/dev/null 2>&1
+ln -s "$LIBRARY_PATH" "$ARDUINO_PATH"
+
+cd $WORKDIR
+FILE="config.h"
+
+get_updates $FILE
+
+echo "Applying customization to:"
+echo "    $FILE..."
+
+# Enable: COREXY
+echo "        Enabling COREXY... \c"
+sed -i -e 's/^\/\/ #define COREXY/#define COREXY/' "$FILE"
+check_return $?
+
+# Change: HOMING_CYCLE_0 to support COREXY
+echo "        Changing HOMING_CYCLE_0 to support COREXY... \c"
+sed -i -e 's/^#define HOMING_CYCLE_0 (1<<Z_AXIS)/#define HOMING_CYCLE_0 ((1<<X_AXIS)|(1<<Y_AXIS))/' "$FILE"
+check_return $?
+
+# Disable: HOMING_CYCLE_1 to support COREXY
+echo "        Disabling HOMING_CYCLE_1 to support COREXY... \c"
+sed -i -e 's/^#define HOMING_CYCLE_1/\/\/#define HOMING_CYCLE_1/' "$FILE"
+check_return $?
+
+# Change: SPINDLE_MAX_RPM to low rotation (tool speed)
+echo "        Changing SPINDLE_MAX_RPM to low rotation (tool speed)... \c"
+sed -i -e 's/^#define SPINDLE_MAX_RPM 1000.0/#define SPINDLE_MAX_RPM 90.0/' "$FILE"
+check_return $?
+
+echo "Changes applied."
+
+# Invoke xc
+echo "Process: xC (Axes Controller): \c"
+case "$action" in
+  'verify')
+    xc "$action" --id escriba --verbosity=3
+    exit $?
+    ;;
+  'upload')
+    xc "$action" --id escriba --verbosity=3
+    exit $?
+    ;;
+  *)
+    echo ""
+    echo "Invalid option."
+    echo ""
+    echo "Usage:"
+    echo "configure.sh verify"
+    echo "configure.sh upload"
+    exit 1
+    ;;
+esac
