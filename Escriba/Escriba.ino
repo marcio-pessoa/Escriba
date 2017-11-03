e5dea2837e93bdc82be55ddc762a264f7650ee91
Marcio Pessoa <marcio.pessoa@sciemon.com>
2017-11-03 19:47:11 -0200

    Arduino code

---
+++ Escriba/Escriba.ino
@@ -0,0 +1,10 @@
+/* x3.ino, Escriba Mark I, Arduino main sketch file
+ * 
+ * This sketch was developed and tested on: Arduino Uno and Yún Shield
+ * To work on other Arduino models, some adaptations may be necessary.
+ * 
+ * Author: Márcio Pessoa <marcio.pessoa@sciemon.com>
+ * Contributors: none
+ */
+
+#include <grbl.h>
