7d8ad7789e05576504998ec8ae49434ce8add3a7
Márcio Pessoa <marcio.pessoa@sciemon.com>
2020-01-01 02:33:23 -0300

    Minor updates

--- Escriba/Escriba.ino
+++ Escriba/Escriba.ino
@@ -1,8 +1,8 @@
 /* x3.ino, Escriba Mark I - Light CNC, Arduino main sketch file
- * 
+ *
  * This sketch was developed and tested on: Arduino Uno and Yún Shield
  * To work on other Arduino models, some adaptations may be necessary.
- * 
+ *
  * Author: Márcio Pessoa <marcio.pessoa@gmail.com>
  * Contributors: none
  */
